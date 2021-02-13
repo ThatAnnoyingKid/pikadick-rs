52518530a3a5354283acdbf20da045f9023acb1d
adumbidiot <nathaniel.daniel23@outlook.com>
2021-02-12 18:26:02 -0800

    Fix rule34

--- lib/rule34-rs/src/types/post.rs
+++ lib/rule34-rs/src/types/post.rs
@@ -13,15 +13,24 @@ use url::Url;
 #[derive(Debug)]
 pub struct Post {
     /// Not included for videos/gifs
+    ///
     pub thumb_url: Option<Url>,
 
     /// Image URL
+    ///
     pub image_url: Url,
 }
 
 impl Post {
-    /// Try to make a Post from a Document
+    /// Try to make a [`Post`] from a [`Document`].
+    ///
     pub fn from_doc(doc: &Document) -> Result<Self, FromDocError> {
+        let options_header = doc
+            .find(Child(Name("div"), Name("h5")))
+            .find(|el| el.find(Text).last().and_then(|el| el.as_text()) == Some("Options"))
+            .and_then(|el| el.parent())
+            .ok_or(FromDocError::MissingOptionsSection)?;
+
         let thumb_url = doc
             .find(Attr("id", "image"))
             .last()
@@ -30,15 +39,15 @@ impl Post {
             .transpose()
             .map_err(FromDocError::InvalidThumbUrl)?;
 
-        let image_url = doc
-            .find(Child(Name("div"), Name("h5")))
-            .find(|el| el.find(Text).last().and_then(|el| el.as_text()) == Some("Options"))
-            .and_then(|el| el.parent())
-            .ok_or(FromDocError::MissingImageUrl)?
+        let image_url = options_header
             .find(Name("li"))
             .find_map(|el| {
                 let a = el.find(Name("a")).last()?;
-                let a_text = el.find(Text).last()?.as_text()?;
+                let a_text = el
+                    .find(Text)
+                    .filter_map(|el| Some(el.as_text()?.trim()))
+                    .filter(|el| !el.is_empty())
+                    .next()?;
                 if a_text == "Original image" {
                     Url::parse(a.attr("href")?).ok()
                 } else {
@@ -53,20 +62,27 @@ impl Post {
         })
     }
 
-    /// Try to get the image name
+    /// Try to get the image name.
+    ///
     pub fn get_image_name(&self) -> Option<&str> {
         self.image_url.path_segments()?.last()
     }
 }
 
-/// Error that may occur while parsing a Post from a Document
+/// Error that may occur while parsing a [`Post`] from a [`Document`].
+///
 #[derive(Debug, thiserror::Error)]
 pub enum FromDocError {
     /// Invalid thumbnail url
+    ///
     #[error("invalid thumbnail url: {0}")]
     InvalidThumbUrl(url::ParseError),
 
+    #[error("missing options section")]
+    MissingOptionsSection,
+
     /// Missing Image Url
+    ///
     #[error("missing image url")]
     MissingImageUrl,
 }
