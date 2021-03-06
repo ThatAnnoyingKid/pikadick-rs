ff32497f1558b250663faed02742166a29a278ea
adumbidiot <nathaniel.daniel23@outlook.com>
2021-03-05 22:34:53 -0800

    Add deviantart-rs

---
+++ lib/deviantart-rs/src/types.rs
@@ -0,0 +1,88 @@
+use std::collections::HashMap;
+use url::Url;
+
+/// DeviantArt Search Results
+///
+#[derive(Debug, serde::Deserialize)]
+pub struct SearchResults {
+    /// Deviations
+    ///
+    pub deviations: Vec<Deviation>,
+
+    /// Unknown K/Vs
+    ///
+    #[serde(flatten)]
+    pub unknown: HashMap<String, serde_json::Value>,
+}
+
+/// A Deviation
+///
+#[derive(Debug, serde::Deserialize)]
+pub struct Deviation {
+    /// DeviantArt Author
+    ///
+    pub author: serde_json::Value,
+
+    /// ?
+    ///
+    #[serde(rename = "blockReasons")]
+    pub block_reasons: Vec<serde_json::Value>,
+
+    /// Deviation ID
+    ///
+    #[serde(rename = "deviationId")]
+    pub deviation_id: u64,
+
+    /// Deviation Type
+    ///
+    #[serde(rename = "type")]
+    pub kind: String,
+
+    /// Image Url
+    ///
+    pub url: Url,
+
+    /// Media Info
+    ///
+    pub media: DeviationMedia,
+
+    /// Unknown K/Vs
+    ///
+    #[serde(flatten)]
+    pub unknown: HashMap<String, serde_json::Value>,
+}
+
+impl Deviation {
+    /// Get the media url for this [`Deviation`].
+    ///
+    pub fn get_media_url(&self) -> Option<Url> {
+        let mut url = self.media.base_uri.clone();
+        url.query_pairs_mut().append_pair(
+            "token",
+            self
+                .media
+                .token
+                .get(0)?,
+        );
+        Some(url)
+    }
+}
+
+/// The media field of a [`Deviation`].
+///
+#[derive(Debug, serde::Deserialize)]
+pub struct DeviationMedia {
+    /// The base uri
+    ///
+    #[serde(rename = "baseUri")]
+    pub base_uri: Url,
+
+    /// Image token
+    ///
+    pub token: Vec<String>,
+
+    /// Unknown K/Vs
+    ///
+    #[serde(flatten)]
+    pub unknown: HashMap<String, serde_json::Value>,
+}
