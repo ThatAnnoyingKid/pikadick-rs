d6865abc5b034756db8c6d96dc33847a5888659b
adumbidiot <nathaniel.daniel23@outlook.com>
2021-03-29 19:06:28 -0700

    Create artifacts dir if non-existent

--- scripts/update.py
+++ scripts/update.py
@@ -1,4 +1,5 @@
 import urllib.request
+import os
 import zipfile
 
 ARTIFACT_URL = 'https://nightly.link/ThatAnnoyingKid/pikadick-rs/workflows/BuildRpi/master/pikadick.zip'
@@ -7,6 +8,13 @@ ZIP_OUT_PATH = OUT_PATH + '/pikadick.zip'
 
 print('Downloading...')
 response = urllib.request.urlopen(ARTIFACT_URL)
+
+# Failures usually mean that it already exists
+try:
+    os.mkdir(OUT_PATH)
+except FileExistsError as error:
+    pass
+    
 
 file = open(ZIP_OUT_PATH, "wb")
 file.write(response.read())
