c0077ada1facf6b927bdcd344d4047a0d1c6138e
adumbidiot <nathaniel.daniel23@outlook.com>
2021-05-10 15:10:15 -0700

    Split r34 cli into 2 subcommands

--- lib/rule34-rs/src/main.rs
+++ lib/rule34-rs/src/main.rs
@@ -4,20 +4,44 @@ use tokio::fs::File;
 
 #[derive(argh::FromArgs)]
 #[argh(description = "A utility to get rule34 images")]
-pub struct Command {
-    #[argh(positional)]
-    #[argh(description = "the query string of the to-be-downloaded image")]
-    query: String,
+pub struct Options {
+    #[argh(subcommand)]
+    subcommand: SubCommand,
+}
 
-    #[argh(option, short = 'o', default = "PathBuf::from(\".\")")]
-    #[argh(description = "the path to save images")]
+#[derive(argh::FromArgs)]
+#[argh(subcommand)]
+enum SubCommand {
+    Search(SearchOptions),
+    Download(DownloadOptions),
+}
+
+#[derive(argh::FromArgs)]
+#[argh(subcommand, name = "search", description = "search for a rule34 post")]
+pub struct SearchOptions {
+    #[argh(positional, description = "the query string")]
+    query: String,
+}
+
+#[derive(argh::FromArgs)]
+#[argh(subcommand, name = "download", description = "download a rule34 post")]
+pub struct DownloadOptions {
+    #[argh(positional, description = "the post id")]
+    id: u64,
+
+    #[argh(
+        option,
+        short = 'o',
+        default = "PathBuf::from(\".\")",
+        description = "the path to save images"
+    )]
     out_dir: PathBuf,
 }
 
 fn main() {
     let exit_code = {
-        let command: Command = argh::from_env();
-        if let Err(e) = real_main(command) {
+        let options: Options = argh::from_env();
+        if let Err(e) = real_main(options) {
             eprintln!("{:?}", e);
             1
         } else {
@@ -28,38 +52,62 @@ fn main() {
     std::process::exit(exit_code);
 }
 
-fn real_main(command: Command) -> anyhow::Result<()> {
+fn real_main(options: Options) -> anyhow::Result<()> {
     let tokio_rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()?;
-    tokio_rt.block_on(async_main(command))
+    tokio_rt.block_on(async_main(options))?;
+    println!("Done.");
+    Ok(())
 }
 
-async fn async_main(command: Command) -> anyhow::Result<()> {
+async fn async_main(options: Options) -> anyhow::Result<()> {
     let client = rule34::Client::new();
-    let results = client.search(&command.query).await?;
 
-    let first_result = results.entries.get(0).context("no results")?;
+    match options.subcommand {
+        SubCommand::Search(options) => {
+            let results = client.search(&options.query).await?;
 
-    let post = client.get_post(first_result.id).await?;
+            if results.entries.is_empty() {
+                println!("No Results");
+            }
 
-    let image_name = post.get_image_name().context("missing image name")?;
-    let current_dir = command.out_dir.join(image_name);
-    tokio::fs::create_dir_all(&command.out_dir).await?;
+            for (i, result) in results.entries.iter().enumerate() {
+                println!("{})", i + 1);
+                println!("ID: {}", result.id);
+                println!("Link: {}", result.link);
+                println!("Description: {}", result.description);
+                println!();
+            }
+        }
+        SubCommand::Download(options) => {
+            let post = client.get_post(options.id).await?;
+            let image_name = post.get_image_name().context("missing image name")?;
+            let out_path = options.out_dir.join(image_name);
 
-    println!("ID: {}", first_result.id);
-    println!("Image Url: {}", post.image_url);
-    println!("Image Name: {}", image_name);
-    println!("Out Dir: {}", current_dir.display());
-    println!();
+            tokio::fs::create_dir_all(&options.out_dir)
+                .await
+                .context("failed to create out dir")?;
 
-    println!("Downloading...");
-    let mut buffer = Vec::with_capacity(1_000_000); // 1 MB
-    client.get_to(&post.image_url, &mut buffer).await?;
+            println!("ID: {}", options.id);
+            println!("Image Url: {}", post.image_url);
+            println!("Image Name: {}", image_name);
+            println!("Out Path: {}", out_path.display());
+            println!();
 
-    let mut file = File::create(current_dir).await?;
-    tokio::io::copy(&mut buffer.as_slice(), &mut file).await?;
-    println!("Done.");
+            println!("Downloading...");
+            let mut buffer = Vec::with_capacity(1_000_000); // 1 MB
+            client
+                .get_to(&post.image_url, &mut buffer)
+                .await
+                .context("failed to download image")?;
+
+            let mut file = File::create(out_path).await?;
+            tokio::io::copy(&mut buffer.as_slice(), &mut file)
+                .await
+                .context("failed to save image")?;
+        }
+    }
 
     Ok(())
 }
