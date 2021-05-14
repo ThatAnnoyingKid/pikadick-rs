1059ff03f7540f692dadf24fea7c671d90d1a26e
adumbidiot <nathaniel.daniel23@outlook.com>
2021-05-13 17:47:45 -0700

    Update upgrade.py script

--- scripts/update.py
+++ scripts/update.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+
 import urllib.request
 import os
 import zipfile
+import stat
 
 ARTIFACT_URL = 'https://nightly.link/ThatAnnoyingKid/pikadick-rs/workflows/BuildRpi/master/pikadick.zip'
 OUT_PATH = 'artifacts'
+OUT_EXE_PATH = OUT_PATH + '/pikadick'
 ZIP_OUT_PATH = OUT_PATH + '/pikadick.zip'
 
 print('Downloading...')
@@ -19,10 +23,19 @@ except FileExistsError as error:
 file = open(ZIP_OUT_PATH, "wb")
 file.write(response.read())
 
+print('Deleting old executable...')
+# Failures usually mean that it is not present
+try:
+    os.remove(OUT_EXE_PATH)
+except FileNotFoundError as error:
+    pass
 
 print('Extracting...')
 zip = zipfile.ZipFile(ZIP_OUT_PATH)
 zip.extractall(path = OUT_PATH)
 zip.close()
+
+print('Making executable...');
+os.chmod(OUT_EXE_PATH, os.stat(OUT_EXE_PATH).st_mode | stat.S_IEXEC)
 
 print('Done.')
