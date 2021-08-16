e5cf77d9ff3c6eff9904c88df50f83d7f14868c1
adumbidiot <nathaniel.daniel23@outlook.com>
2021-08-15 19:45:56 -0700

    Add file support

--- lib/sauce-nao-rs/src/lib.rs
+++ lib/sauce-nao-rs/src/lib.rs
@@ -1,7 +1,12 @@
+/// Image type
+pub mod image;
 /// Api types
 pub mod types;
 
-pub use self::types::SearchJson;
+pub use self::{
+    image::Image,
+    types::SearchJson,
+};
 use std::sync::Arc;
 use url::Url;
 
@@ -34,19 +39,29 @@ impl Client {
     }
 
     /// Look up an image
-    pub async fn search(&self, url: &str) -> Result<SearchJson, Error> {
-        let url = Url::parse_with_params(
+    pub async fn search(&self, image: impl Into<Image>) -> Result<SearchJson, Error> {
+        let image = image.into();
+        let mut url = Url::parse_with_params(
             "https://saucenao.com/search.php?output_type=2",
-            &[("api_key", &*self.api_key), ("url", url)],
+            &[("api_key", &*self.api_key)],
         )?;
-        Ok(self
-            .client
-            .get(url.as_str())
-            .send()
-            .await?
-            .error_for_status()?
-            .json()
-            .await?)
+
+        let mut part = None;
+        match image {
+            Image::Url(image_url) => {
+                url.query_pairs_mut().append_pair("url", &image_url);
+            }
+            Image::File { name, body } => {
+                part = Some(reqwest::multipart::Part::stream(body).file_name(name));
+            }
+        }
+
+        let mut res = self.client.post(url.as_str());
+        if let Some(part) = part {
+            let form = reqwest::multipart::Form::new().part("file", part);
+            res = res.multipart(form);
+        }
+        Ok(res.send().await?.error_for_status()?.json().await?)
     }
 }
 
@@ -55,15 +70,27 @@ mod tests {
     use super::*;
 
     const API_KEY: &str = include_str!("../api_key.txt");
+    const IMAGE_PATH: &str = "./test_data/oZjCxGo.jpg";
 
     #[tokio::test]
     #[ignore]
-    async fn search_works() {
+    async fn search_url_works() {
         let client = Client::new(API_KEY);
         let results = client
             .search("https://i.imgur.com/oZjCxGo.jpg")
             .await
             .expect("failed to search");
+        dbg!(results);
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn search_file_works() {
+        let image = Image::from_path(IMAGE_PATH.as_ref())
+            .await
+            .expect("failed to open image");
+        let client = Client::new(API_KEY);
+        let results = client.search(image).await.expect("failed to search");
         dbg!(results);
     }
 }
