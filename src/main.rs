2b1462189f9ac59400b74deee839aa4d74f49bfb
adumbidiot <nathaniel.daniel23@outlook.com>
2021-08-18 01:06:18 -0700

    Fix build

--- src/main.rs
+++ src/main.rs
@@ -441,6 +441,7 @@ async fn async_main(config: Config, missing_data_dir: bool) {
         .help(&HELP)
         .group(&GENERAL_GROUP)
         .bucket("nekos", |b| b.delay(1))
+        .await
         .bucket("r6stats", |b| b.delay(7))
         .await
         .bucket("r6tracker", |b| b.delay(7))
