b0ca3f7f15c802921aabd710a49e852a2a3de228
adumbidiot <nathaniel.daniel23@outlook.com>
2021-08-26 13:50:18 -0700

    Clean up nekos lib

--- lib/nekos-rs/src/client.rs
+++ lib/nekos-rs/src/client.rs
@@ -1,9 +1,11 @@
 use crate::{
     types::ImageList,
-    NekosError,
-    NekosResult,
+    Error,
 };
-use std::io::Write;
+use tokio::io::{
+    AsyncWrite,
+    AsyncWriteExt,
+};
 use url::Url;
 
 const DEFAULT_USER_AGENT: &str = "nekos-rs";
@@ -23,44 +25,35 @@ impl Client {
     }
 
     /// Get a random list of catgirls.
+    ///
     /// count is a num from 0 < count <= 100 and is the number of returned images.
     /// nsfw is whether the images should be nsfw. If not specified, both are returned.
-    pub async fn get_random(&self, nsfw: Option<bool>, count: u8) -> NekosResult<ImageList> {
+    pub async fn get_random(&self, nsfw: Option<bool>, count: u8) -> Result<ImageList, Error> {
         let mut buf = itoa::Buffer::new();
         let count_query = std::iter::once(("count", buf.format(count.min(100))));
         let nsfw_query = nsfw.map(|nsfw| ("nsfw", if nsfw { "true" } else { "false" }));
         let query = count_query.chain(nsfw_query);
         let url = Url::parse_with_params("https://nekos.moe/api/v1/random/image", query)?;
 
-        let res = self
+        Ok(self
             .client
             .get(url.as_str())
             .header(reqwest::header::USER_AGENT, DEFAULT_USER_AGENT)
             .send()
-            .await?;
-
-        let status = res.status();
-        if !status.is_success() {
-            return Err(NekosError::InvalidStatus(status));
-        }
-
-        let body = res.text().await?;
-
-        let json = serde_json::from_str(&body)?;
-
-        Ok(json)
+            .await?
+            .error_for_status()?
+            .json()
+            .await?)
     }
 
     /// Get a url and copy it to the given writer
-    pub async fn copy_res_to<T: Write>(&self, url: &Url, mut writer: T) -> NekosResult<()> {
-        let mut res = self.client.get(url.as_str()).send().await?;
-        let status = res.status();
-        if !status.is_success() {
-            return Err(NekosError::InvalidStatus(status));
-        }
-
+    pub async fn get_to_writer<W>(&self, url: &str, mut writer: W) -> Result<(), Error>
+    where
+        W: AsyncWrite + Unpin,
+    {
+        let mut res = self.client.get(url).send().await?.error_for_status()?;
         while let Some(chunk) = res.chunk().await? {
-            writer.write_all(&chunk)?;
+            writer.write_all(&chunk).await?;
         }
 
         Ok(())
