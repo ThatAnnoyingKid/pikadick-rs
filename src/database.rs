e61ff61e50bfced6a966593f0513f05de4e5912f
adumbidiot <nathaniel.daniel23@outlook.com>
2021-10-12 16:51:21 -0700

    Move KV store to separate file

--- src/database.rs
+++ src/database.rs
@@ -1,3 +1,4 @@
+mod kv_store;
 pub mod model;
 
 use crate::database::model::{
@@ -20,10 +21,6 @@ use std::{
 
 // Setup
 const SETUP_TABLES_SQL: &str = include_str!("../sql/setup_tables.sql");
-
-// K/V Store
-const GET_STORE_SQL: &str = include_str!("../sql/get_store.sql");
-const PUT_STORE_SQL: &str = include_str!("../sql/put_store.sql");
 
 // Tic-Tac-Toe
 const DELETE_TIC_TAC_TOE_GAME_SQL: &str = include_str!("../sql/delete_tic_tac_toe_game.sql");
@@ -207,95 +204,6 @@ impl Database {
             .map(|row| row.unwrap_or(false))
         })
         .await?
-    }
-
-    /// Get a key from the store
-    pub async fn store_get<P, K, V>(&self, prefix: P, key: K) -> anyhow::Result<Option<V>>
-    where
-        P: AsRef<[u8]>,
-        K: AsRef<[u8]>,
-        V: serde::de::DeserializeOwned,
-    {
-        let prefix = prefix.as_ref().to_vec();
-        let key = key.as_ref().to_vec();
-
-        let maybe_bytes: Option<Vec<u8>> = self
-            .access_db(move |db| {
-                db.prepare_cached(GET_STORE_SQL)?
-                    .query_row([prefix, key], |row| row.get(0))
-                    .optional()
-                    .context("failed to get value")
-            })
-            .await??;
-
-        match maybe_bytes {
-            Some(bytes) => Ok(Some(
-                bincode::deserialize(&bytes).context("failed to decode value")?,
-            )),
-            None => Ok(None),
-        }
-    }
-
-    /// Put a key in the store
-    pub async fn store_put<P, K, V>(&self, prefix: P, key: K, value: V) -> anyhow::Result<()>
-    where
-        P: AsRef<[u8]>,
-        K: AsRef<[u8]>,
-        V: serde::Serialize,
-    {
-        let prefix = prefix.as_ref().to_vec();
-        let key = key.as_ref().to_vec();
-        let value = bincode::serialize(&value).context("failed to serialize value")?;
-
-        self.access_db(move |db| {
-            let txn = db.transaction()?;
-            txn.prepare_cached(PUT_STORE_SQL)?
-                .execute(params![prefix, key, value])?;
-            txn.commit().context("failed to insert key into kv_store")
-        })
-        .await??;
-
-        Ok(())
-    }
-
-    /// Get and Put a key in the store in one action, ensuring the key is not changed between the commands.
-    pub async fn store_update<P, K, V, U>(
-        &self,
-        prefix: P,
-        key: K,
-        update_func: U,
-    ) -> anyhow::Result<()>
-    where
-        P: AsRef<[u8]>,
-        K: AsRef<[u8]>,
-        V: serde::Serialize + serde::de::DeserializeOwned,
-        U: FnOnce(Option<V>) -> V + Send + 'static,
-    {
-        let prefix = prefix.as_ref().to_vec();
-        let key = key.as_ref().to_vec();
-
-        self.access_db(move |db| {
-            let txn = db.transaction_with_behavior(TransactionBehavior::Immediate)?;
-
-            let maybe_value = txn
-                .prepare_cached(GET_STORE_SQL)?
-                .query_row(params![prefix, key], |row| row.get(0))
-                .optional()
-                .context("failed to get value")?
-                .map(|bytes: Vec<u8>| {
-                    bincode::deserialize(&bytes).context("failed to decode value")
-                })
-                .transpose()?;
-            let value = update_func(maybe_value);
-            let value = bincode::serialize(&value).context("failed to serialize value")?;
-
-            txn.prepare_cached(PUT_STORE_SQL)?
-                .execute(params![prefix, key, value])?;
-            txn.commit().context("failed to insert key into kv_store")
-        })
-        .await??;
-
-        Ok(())
     }
 
     /// Enable or disable reddit embeds.
