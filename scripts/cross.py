55d55498dbc820ce98bee17ff94e484262466824
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2021-12-21 23:35:56 -0800

    Fix cross (#233)
    * Fix cross
    
    * Update to serenity-next
    
    * fix warning
    
    * Make `cross.py` script depend on `across-rs`

--- scripts/cross.py
+++ scripts/cross.py
@@ -1,23 +1,11 @@
 import os
 import subprocess
-import tomli
 
 ##############
 ### CONFIG ###
 ##############
 
-# Hardcode for now
 TARGET = "armv7-unknown-linux-gnueabihf"
-
-# Run 'strip' on the final binary
-USE_STRIP = True
-
-#################
-### INTERNALS ###
-#################
-
-# Note: Ignores previously set RUSTFLAGS. Extend this script if you want more.
-RUSTFLAGS = ""
 
 #############
 ### SETUP ###
@@ -28,14 +16,6 @@ is_ci = os.getenv("CI") is not None
 cross_compile_info_file_name = "cross-compile-info.toml"
 if is_ci:
     cross_compile_info_file_name = "cross-compile-info.ci.toml"
-cross_compile_info_file_name = os.path.join(os.getcwd(), cross_compile_info_file_name)
-print("Parsing `{}`".format(cross_compile_info_file_name))
-cross_compile_info_file = open(cross_compile_info_file_name, encoding="utf-8")
-cross_compile_info = tomli.load(cross_compile_info_file)
-
-linker = cross_compile_info[TARGET]['linker']
-strip_bin = cross_compile_info[TARGET]['strip']
-RUSTFLAGS += "-Clinker={} ".format(linker)
 
 # These are necessary for some reason?
 # RUSTFLAGS += "-Clink-args=-Xlinker -rpath=/usr/lib/arm-linux-gnueabihf " 
@@ -47,20 +27,4 @@ RUSTFLAGS += "-Clinker={} ".format(linker)
 ###############
 ### RUNNING ###
 ###############
-print("Compiling")
-cmd_list = [ "cargo", "build" ]
-cmd_list.extend([ "--target", TARGET ])
-cmd_list.extend([ "--features", "use-openssl-vendored" ])
-cmd_list.extend([ "--release" ])
-
-env = {}
-env.update(os.environ)
-env.update(cross_compile_info[TARGET]['env'])
-env["RUSTFLAGS"] = RUSTFLAGS
-env["RUST_BACKTRACE"] = "1"
-
-subprocess.call(cmd_list, env=env)
-
-print("Stripping")
-if USE_STRIP:
-    subprocess.call([ strip_bin, 'target/{}/release/pikadick'.format(TARGET) ])
+subprocess.run(f'cargo run -p across --release -- --config {cross_compile_info_file_name} --target {TARGET} --features use-openssl-vendored --use-strip --release', check=True)
