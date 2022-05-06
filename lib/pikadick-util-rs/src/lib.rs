6fb951bba7fe5863d74eea42be9ce56505cf8851
adumbidiot <nathaniel.daniel23@outlook.com>
2022-05-06 16:07:49 -0700

    Move function to util crate

---
+++ lib/pikadick-util-rs/src/lib.rs
@@ -0,0 +1,4 @@
+#[cfg(feature = "download_to_file")]
+mod download_to_file;
+#[cfg(feature = "download_to_file")]
+pub use self::download_to_file::download_to_file;
--- src/util.rs
+++ src/util.rs
@@ -16,60 +16,33 @@ pub use self::{
         TimedCacheEntry,
     },
 };
-use anyhow::{
-    ensure,
-    Context,
+pub use pikadick_util::download_to_file;
+use std::{
+    ffi::{
+        OsStr,
+        OsString,
+    },
+    path::PathBuf,
 };
-use tokio::io::AsyncWriteExt;
 
-/// Download a url using a GET request to a tokio file.
-pub async fn download_to_file(
-    client: &reqwest::Client,
-    url: &str,
-    file: &mut tokio::fs::File,
-) -> anyhow::Result<()> {
-    // Send the request
-    let mut response = client
-        .get(url)
-        .send()
-        .await
-        .context("failed to send request")?
-        .error_for_status()
-        .context("invalid http status")?;
+/// Push an extension to a [`PathBuf`].
+pub fn push_extension<S: AsRef<OsStr>>(path: &mut PathBuf, extension: S) {
+    let extension = extension.as_ref();
 
-    // Pre-allocate file space if possible.
-    let content_length = response.content_length();
-    if let Some(content_length) = content_length {
-        file.set_len(content_length)
-            .await
-            .context("failed to pre-allocate file")?;
+    // Bail out early if there is no extension, simply setting one.
+    if path.extension().is_none() {
+        path.set_extension(extension);
+        return;
     }
 
-    // Keep track of the file size in case the server lies
-    let mut actual_length = 0;
-
-    // Download the file chunk-by-chunk
-    while let Some(chunk) = response.chunk().await.context("failed to get next chunk")? {
-        file.write_all(&chunk)
-            .await
-            .context("failed to write to file")?;
-        actual_length +=
-            u64::try_from(chunk.len()).context("failed to convert chunk size to `u64`")?;
-    }
-
-    // Ensure file size matches content_length
-    if let Some(content_length) = content_length {
-        ensure!(
-            content_length == actual_length,
-            "content-length mismatch, {} != {}",
-            content_length,
-            actual_length
-        );
-    }
-
-    // Sync data
-    file.flush().await.context("failed to flush file")?;
-    file.sync_all().await.context("failed to sync file data")?;
-
-    Ok(())
+    // Take the path memory, make it a string, push the extension, and restore the argument path.
+    //
+    // Ideally, I woudln't take ownership of the original string,
+    // but there is no API to push arbitrary bytes to a [`PathBuf`].
+    // Similarly, there is no api to access the underlying [`OsString`] of a [`PathBuf`].
+    let mut path_string = OsString::from(std::mem::take(path));
+    path_string.reserve(extension.len() + 1);
+    path_string.push(".");
+    path_string.push(extension);
+    std::mem::swap(path, &mut path_string.into());
 }
