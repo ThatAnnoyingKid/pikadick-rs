1d2c2d94acdf0a0422ec4792b408caeab005af18
adumbidiot <nathaniel.daniel23@outlook.com>
2022-05-10 20:22:24 -0700

    Move `AsyncLockFile` to `pikadick-util`

--- lib/pikadick-util-rs/src/lib.rs
+++ lib/pikadick-util-rs/src/lib.rs
@@ -8,6 +8,11 @@ mod arc_anyhow_error;
 #[cfg(feature = "arc_anyhow_error")]
 pub use self::arc_anyhow_error::ArcAnyhowError;
 
+#[cfg(feature = "async_lock_file")]
+mod async_lock_file;
+#[cfg(feature = "async_lock_file")]
+pub use self::async_lock_file::AsyncLockFile;
+
 use std::{
     ffi::{
         OsStr,
