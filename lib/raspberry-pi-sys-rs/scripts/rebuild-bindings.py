6fa90cfac99b8ab3edbac0bf8249964b9ced15f1
adumbidiot <nathaniel.daniel23@outlook.com>
2022-05-29 18:14:08 -0700

    Allow skipping apt download

--- lib/raspberry-pi-sys-rs/scripts/rebuild-bindings.py
+++ lib/raspberry-pi-sys-rs/scripts/rebuild-bindings.py
@@ -5,6 +5,7 @@ import tarfile
 import unix_ar
 import os
 import subprocess
+import argparse
 
 HOST = "https://archive.raspberrypi.org"
 DIST = "bullseye"
@@ -97,40 +98,45 @@ def generate_bindings(arch):
 		raise Exception(f'unsupported arch `{arch}`')
 	
 	# libbcm_host
-	subprocess.run(f'bindgen bindgen-bcm_host.h -o src/{bindings_directory}/libbcm_host.rs --allowlist-function bcm_host_.* --allowlist-function graphics_get_display_size --allowlist-var BCM_HOST_.* --allowlist-function vc_gencmd_.* --allowlist-var GENCMDSERVICE_MSGFIFO_SIZE --blocklist-function vc_gencmd_inum --blocklist-function vc_gencmd_read_response_partial --blocklist-function vc_gencmd_close_response_partial --blocklist-function vc_gencmd_read_partial_state --allowlist-function vcos_.* --blocklist-function vcos_pthreads_timer_reset --blocklist-function vcos_kmalloc --blocklist-function vcos_kcalloc --blocklist-function vcos_kfree --blocklist-function vcos_log_set_level_all --dynamic-loading libbcm_host --blocklist-function vcos_log_assert_cmd --blocklist-function vcos_log_set_cmd --blocklist-function vcos_log_status_cmd --blocklist-function vcos_log_test_cmd --blocklist-function vc_dispman_init --blocklist-function vc_dispmanx_resource_write_data_handle --dynamic-link-require-all -- --target={clang_target} --sysroot=bundled/{arch} -Ibundled/{arch}/usr/include', check=True)
+	subprocess.run(f'bindgen bindgen-bcm_host.h -o src/{bindings_directory}/libbcm_host.rs --allowlist-function bcm_host_.* --allowlist-function graphics_get_display_size --allowlist-var BCM_HOST_.* --allowlist-function vc_gencmd_.* --allowlist-var GENCMDSERVICE_MSGFIFO_SIZE --blocklist-function vc_gencmd_inum --blocklist-function vc_gencmd_read_response_partial --blocklist-function vc_gencmd_close_response_partial --blocklist-function vc_gencmd_read_partial_state --allowlist-function vcos_.* --blocklist-function vcos_pthreads_timer_reset --blocklist-function vcos_kmalloc --blocklist-function vcos_kcalloc --blocklist-function vcos_kfree --blocklist-function vcos_log_set_level_all --dynamic-loading libbcm_host --blocklist-function vcos_log_assert_cmd --blocklist-function vcos_log_set_cmd --blocklist-function vcos_log_status_cmd --blocklist-function vcos_log_test_cmd --blocklist-function vc_dispman_init --blocklist-function vc_dispmanx_resource_write_data_handle --allowlist-function vchi_.* --blocklist-function vchi_crc_control --dynamic-link-require-all -- --target={clang_target} --sysroot=bundled/{arch} -Ibundled/{arch}/usr/include', check=True)
 
 def main():
-	apt_package_downloader = AptPackageDownloader()
+	parser = argparse.ArgumentParser(description='Rebuild bindings')
+	parser.add_argument('--skip-apt', action='store_true', help='skip downloading apt packages')
+	args = parser.parse_args()
 	
-	armhf_dir = "bundled/armhf"
-	arm64_dir = "bundled/arm64"
-	
-	print('Getting packages list...')
-	package_list_armhf = apt_package_downloader.get_package_list('armhf')
-	print(f'Got {len(package_list_armhf)} armhf packages')
-	package_list_arm64 = apt_package_downloader.get_package_list('arm64')
-	print(f'Got {len(package_list_arm64)} arm64 packages')
-	
-	try:
-		os.mkdir(armhf_dir)
-	except FileExistsError:
-		pass
-	try:
-		os.mkdir(arm64_dir)
-	except FileExistsError:
-		pass
+	if not args.skip_apt:
+		apt_package_downloader = AptPackageDownloader()
 		
-	needed_packages = [
-		"libraspberrypi-dev",
-		"libc6-dev",
-		"linux-libc-dev",
-	]
-	
-	for package in needed_packages:
-		print(f'Installing `{package}` (armhf)...')
-		install_deb(apt_package_downloader, 'armhf', package, armhf_dir)
-		print(f'Installing `{package}` (arm64)...')
-		install_deb(apt_package_downloader, 'arm64', package, arm64_dir)
+		armhf_dir = "bundled/armhf"
+		arm64_dir = "bundled/arm64"
+		
+		print('Getting packages list...')
+		package_list_armhf = apt_package_downloader.get_package_list('armhf')
+		print(f'Got {len(package_list_armhf)} armhf packages')
+		package_list_arm64 = apt_package_downloader.get_package_list('arm64')
+		print(f'Got {len(package_list_arm64)} arm64 packages')
+		
+		try:
+			os.mkdir(armhf_dir)
+		except FileExistsError:
+			pass
+		try:
+			os.mkdir(arm64_dir)
+		except FileExistsError:
+			pass
+			
+		needed_packages = [
+			"libraspberrypi-dev",
+			"libc6-dev",
+			"linux-libc-dev",
+		]
+		
+		for package in needed_packages:
+			print(f'Installing `{package}` (armhf)...')
+			install_deb(apt_package_downloader, 'armhf', package, armhf_dir)
+			print(f'Installing `{package}` (arm64)...')
+			install_deb(apt_package_downloader, 'arm64', package, arm64_dir)
 		
 	print('Generating bindings (armhf)...')
 	generate_bindings("armhf")
