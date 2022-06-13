95faa2f044c7c60c01fbd3deecac5c255d5bc657
adumbidiot <nathaniel.daniel23@outlook.com>
2022-06-12 17:21:21 -0700

    Pretty-print boot time in tests

--- lib/pikadick-system-info-rs/src/lib.rs
+++ lib/pikadick-system-info-rs/src/lib.rs
@@ -20,10 +20,19 @@ pub fn get_boot_time() -> SystemTime {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::time::Instant;
 
     #[test]
     fn boot_time() {
+        let offset = time::UtcOffset::current_local_offset().expect("failed to get local offset");
+        
+        let start = Instant::now();
         let boot_time = get_boot_time();
-        println!("Boot Time: {}", time::OffsetDateTime::from(boot_time));
+        let elapsed = start.elapsed();
+        println!(
+            "Boot Time: {}\nTime: {:?}",
+            time::OffsetDateTime::from(boot_time).to_offset(offset),
+            elapsed
+        );
     }
 }
