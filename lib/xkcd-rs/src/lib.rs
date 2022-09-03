df3330e4d0570c1beded8ac60c06864b4e5a8ca3
adumbidiot <nathaniel.daniel23@outlook.com>
2022-09-02 17:54:17 -0700

    Update xkcd lib

--- lib/xkcd-rs/src/lib.rs
+++ lib/xkcd-rs/src/lib.rs
@@ -1,22 +1,14 @@
 use reqwest::Url;
 
 /// Library Error type
-///
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// Rewqwest HTTP Error
-    ///
-    #[error("{0}")]
+    #[error(transparent)]
     Reqwest(#[from] reqwest::Error),
-
-    /// Invalid HTTP StatusCode
-    ///
-    #[error("{0}")]
-    InvalidStatus(reqwest::StatusCode),
 }
 
 /// An XKCD client
-///
 #[derive(Debug, Clone)]
 pub struct Client {
     client: reqwest::Client,
@@ -24,7 +16,6 @@ pub struct Client {
 
 impl Client {
     /// Make a new [`Client`].
-    ///
     pub fn new() -> Self {
         Client {
             client: reqwest::Client::new(),
@@ -32,18 +23,19 @@ impl Client {
     }
 
     /// Get a random xkcd comic url.
-    ///
     pub async fn get_random(&self) -> Result<Url, Error> {
+        // We can't use head, we get rejected with 405.
         let res = self
             .client
             .get("https://c.xkcd.com/random/comic/")
             .send()
-            .await?;
-        let status = res.status();
-        if !status.is_success() {
-            return Err(Error::InvalidStatus(status));
-        }
+            .await?
+            .error_for_status()?;
+        // Stash the final url we get redirected to.
         let ret = res.url().clone();
+
+        // We do this to be nice,
+        // and not just cancel the body as some servers may freak out.
         let _body = res.text().await?;
 
         Ok(ret)
@@ -64,6 +56,6 @@ mod tests {
     async fn it_works() {
         let client = Client::new();
         let result = client.get_random().await.expect("failed to get xkcd comic");
-        dbg!(result);
+        dbg!(&result);
     }
 }
